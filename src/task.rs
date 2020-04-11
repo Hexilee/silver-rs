a3ff560117028c9a5523fa1f0da1371dda3f6cc2
Hexilee <hexileee@gmail.com>
2020-04-11 12:43:04 +0800

    disable recursively calling block_on

--- src/task.rs
+++ src/task.rs
@@ -1,6 +1,7 @@
 use async_task::waker_fn;
 use crossbeam_utils::sync::Parker;
 use futures::pin_mut;
+use std::cell::Cell;
 use std::future::Future;
 use std::task::{Context, Poll, Waker};
 
@@ -15,10 +16,33 @@ where
             let waker = waker_fn(move || unparker.unpark());
             (parker, waker)
         };
-    }
-    pin_mut!(fut);
 
-    SCHEDULE.with(|(parker, waker): &(Parker, Waker)| {
+        static IS_RUNNING: Cell<bool> = Cell::new(false);
+    }
+
+    struct RunningGuard;
+
+    impl RunningGuard {
+        fn new() -> Self {
+            IS_RUNNING.with(|is_running| {
+                if is_running.get() {
+                    panic!("recursively call `block_on`")
+                }
+                is_running.set(true)
+            });
+            Self
+        }
+    }
+
+    impl Drop for RunningGuard {
+        fn drop(&mut self) {
+            IS_RUNNING.with(|is_running| is_running.set(false));
+        }
+    }
+
+    pin_mut!(fut);
+    let _guard = RunningGuard::new();
+    SCHEDULE.with(|(parker, waker)| {
         let mut ctx = Context::from_waker(waker);
         loop {
             match fut.as_mut().poll(&mut ctx) {
@@ -27,4 +51,17 @@ where
             }
         }
     })
+}
+
+#[cfg(any(test, bench))]
+mod tests {
+    use super::block_on;
+
+    #[test]
+    #[should_panic]
+    fn recursive_block_on() {
+        block_on(async {
+            block_on(async {});
+        });
+    }
 }
