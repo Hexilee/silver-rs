9fcb9a42225da659ad7f663cf06eda26f3afeab3
Hexilee <hexileee@gmail.com>
2020-04-14 17:32:07 +0800

    use unparker queue in executor pool

--- src/task/spawn.rs
+++ src/task/spawn.rs
@@ -1,5 +1,7 @@
 use super::{JoinHandle, Task};
 use crossbeam_deque::{Injector, Stealer, Worker};
+use crossbeam_queue::ArrayQueue;
+use crossbeam_utils::sync::{Parker, Unparker};
 use futures::FutureExt;
 use once_cell::sync::Lazy;
 use std::future::Future;
@@ -7,7 +9,51 @@ use std::iter;
 use std::panic::AssertUnwindSafe;
 use std::sync::Arc;
 use std::thread;
-use std::time::Duration;
+
+struct Pool {
+    injector: Injector<Task>,
+    unparkers: ArrayQueue<Unparker>,
+}
+
+impl Pool {
+    fn new(cpus: usize) -> Self {
+        Self {
+            injector: Injector::new(),
+            unparkers: ArrayQueue::new(cpus),
+        }
+    }
+}
+
+static POOL: Lazy<Arc<Pool>> = Lazy::new(|| {
+    let nums = num_cpus::get();
+    let pool = Arc::new(Pool::new(nums));
+    let workers = (0..nums).map(|_| Worker::new_fifo()).collect::<Vec<_>>();
+    let stealers = workers
+        .iter()
+        .map(|worker| worker.stealer())
+        .collect::<Vec<Stealer<Task>>>();
+    for (index, worker) in workers.into_iter().enumerate() {
+        let stealers = stealers.clone();
+        let pool = pool.clone();
+        thread::Builder::new()
+            .name(format!("tio/async{}", index))
+            .spawn(move || {
+                let parker = Parker::new();
+                loop {
+                    if let Some(task) = find_task(&worker, &pool.injector, &stealers) {
+                        task.run();
+                    } else {
+                        pool.unparkers
+                            .push(parker.unparker().clone())
+                            .expect("fail to push unparker");
+                        parker.park()
+                    }
+                }
+            })
+            .expect("fail to start thread");
+    }
+    pool
+});
 
 fn find_task<T>(local: &Worker<T>, global: &Injector<T>, stealers: &[Stealer<T>]) -> Option<T> {
     // Pop a task from the local queue, if not empty.
@@ -27,40 +73,20 @@ fn find_task<T>(local: &Worker<T>, global: &Injector<T>, stealers: &[Stealer<T>]
     })
 }
 
-const TIMEOUT: Duration = Duration::from_micros(80);
-
-static POOL: Lazy<Arc<Injector<Task>>> = Lazy::new(|| {
-    let injector = Arc::new(Injector::new());
-    let nums = num_cpus::get();
-    let workers = (0..nums).map(|_| Worker::new_fifo()).collect::<Vec<_>>();
-    let stealers = workers
-        .iter()
-        .map(|worker| worker.stealer())
-        .collect::<Vec<Stealer<Task>>>();
-    for (index, worker) in workers.into_iter().enumerate() {
-        let injector = injector.clone();
-        let stealers = stealers.clone();
-        thread::Builder::new()
-            .name(format!("tio/async{}", index))
-            .spawn(move || loop {
-                if let Some(task) = find_task(&worker, &injector, &stealers) {
-                    task.run()
-                } else {
-                    thread::sleep(TIMEOUT)
-                }
-            })
-            .expect("fail to start thread");
+fn schedule(t: Task) {
+    POOL.injector.push(t);
+    if let Ok(unparker) = POOL.unparkers.pop() {
+        unparker.unpark()
     }
-    injector
-});
+}
 
 pub fn spawn<F, R>(fut: F) -> JoinHandle<R>
 where
     R: 'static + Send,
     F: 'static + Send + Future<Output = R>,
 {
-    let (task, handler) =
-        async_task::spawn(AssertUnwindSafe(fut).catch_unwind(), |f| POOL.push(f), ());
+    let fut = AssertUnwindSafe(fut).catch_unwind();
+    let (task, handler) = async_task::spawn(fut, schedule, ());
     task.schedule();
     JoinHandle(handler)
 }
