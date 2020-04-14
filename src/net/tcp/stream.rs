d855bb1bb544e8f851498ccea4a3d8f9c180bca6
Hexilee <hexileee@gmail.com>
2020-04-15 02:07:15 +0800

    complete documenting for methods of TcpStream

--- src/net/tcp/stream.rs
+++ src/net/tcp/stream.rs
@@ -1,10 +1,10 @@
 use crate::net::poll::Watcher;
 use futures::task::{Context, Poll};
-use futures::{AsyncRead, AsyncWrite};
+use futures::{future, AsyncRead, AsyncWrite};
 use mio::net;
 use std::convert::TryFrom;
 use std::io::{self, IoSlice, IoSliceMut, Read, Write};
-use std::net::{SocketAddr, TcpStream as StdStream};
+use std::net::{SocketAddr, TcpStream as StdStream, ToSocketAddrs};
 use std::pin::Pin;
 use std::sync::Arc;
 
@@ -12,15 +12,256 @@ use std::sync::Arc;
 pub struct TcpStream(Arc<Watcher<net::TcpStream>>);
 
 impl TcpStream {
-    pub async fn connect(addr: SocketAddr) -> io::Result<Self> {
+    /// Connect to a socket addr
+    async fn connect_once(addr: SocketAddr) -> io::Result<Self> {
         let watcher = Watcher::with(net::TcpStream::connect(addr)?)?;
         let inner = Arc::new(watcher);
-        futures::future::poll_fn(|cx| inner.poll_write_with(cx, |mut o| o.write(b"".as_ref())))
-            .await?;
+        future::poll_fn(|cx| inner.poll_write_with(cx, |mut o| o.write("".as_bytes()))).await?;
         match inner.take_error() {
             Ok(None) => Ok(Self(inner)),
             Ok(Some(err)) | Err(err) => Err(err),
         }
+    }
+
+    /// Creates a new TCP stream connected to the specified address.
+    ///
+    /// This method will create a new TCP socket and attempt to connect it to the `addr`
+    /// provided. The returned future will be resolved once the stream has successfully
+    /// connected, or it will return an error if one occurs.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// # fn main() -> std::io::Result<()> { tio::task::block_on(async {
+    /// #
+    /// use tio::net::TcpStream;
+    ///
+    /// let stream = TcpStream::connect("127.0.0.1:0").await?;
+    /// #
+    /// # Ok(()) }) }
+    /// ```
+    ///
+    /// # Blocking
+    ///
+    /// This method may be blocked by addrs resolving.
+    /// You can construct a [`std::net::TcpStream`] by [`spawn_blocking`],
+    /// then convert it to [`TcpStream`] by [`std::convert::TryInto`].
+    ///
+    /// [`spawn_blocking`]: ../task/fn.spawn_blocking.html
+    /// [`std::net::TcpStream`]: https://doc.rust-lang.org/std/net/struct.TcpStream.html
+    /// [`std::convert::TryInto`]: https://doc.rust-lang.org/std/convert/trait.TryInto.html
+    /// ```no_run
+    /// # fn main() -> std::io::Result<()> { tio::task::block_on(async {
+    /// #
+    /// use tio::net::TcpStream;
+    /// use std::net::TcpStream as StdStream;
+    /// use std::convert::TryInto;
+    /// use tio::task::spawn_blocking;
+    ///
+    /// let stream: TcpStream = spawn_blocking(|| StdStream::connect("github.com"))
+    ///     .await?.try_into()?;
+    ///
+    /// # Ok(()) }) }
+    /// ```
+    pub async fn connect(addrs: impl ToSocketAddrs) -> io::Result<Self> {
+        let mut error = None;
+        for addr in addrs.to_socket_addrs()? {
+            match Self::connect_once(addr).await {
+                Err(err) => {
+                    error = Some(err);
+                }
+                ok => return ok,
+            }
+        }
+        Err(error.unwrap_or_else(|| {
+            io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "could not resolve to any addresses",
+            )
+        }))
+    }
+    /// Returns the local address that this stream is connected to.
+    ///
+    /// ## Examples
+    ///
+    /// ```no_run
+    /// # fn main() -> std::io::Result<()> { tio::task::block_on(async {
+    /// #
+    /// use tio::net::TcpStream;
+    ///
+    /// let stream = TcpStream::connect("127.0.0.1:8080").await?;
+    /// let addr = stream.local_addr()?;
+    /// #
+    /// # Ok(()) }) }
+    /// ```
+    pub fn local_addr(&self) -> io::Result<SocketAddr> {
+        self.0.local_addr()
+    }
+
+    /// Returns the remote address that this stream is connected to.
+    ///
+    /// ## Examples
+    ///
+    /// ```no_run
+    /// # fn main() -> std::io::Result<()> { tio::task::block_on(async {
+    /// #
+    /// use tio::net::TcpStream;
+    ///
+    /// let stream = TcpStream::connect("127.0.0.1:8080").await?;
+    /// let peer = stream.peer_addr()?;
+    /// #
+    /// # Ok(()) }) }
+    /// ```
+    pub fn peer_addr(&self) -> io::Result<SocketAddr> {
+        self.0.peer_addr()
+    }
+
+    /// Gets the value of the `IP_TTL` option for this socket.
+    ///
+    /// For more information about this option, see [`set_ttl`].
+    ///
+    /// [`set_ttl`]: #method.set_ttl
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// # fn main() -> std::io::Result<()> { tio::task::block_on(async {
+    /// #
+    /// use tio::net::TcpStream;
+    ///
+    /// let stream = TcpStream::connect("127.0.0.1:8080").await?;
+    ///
+    /// stream.set_ttl(100)?;
+    /// assert_eq!(stream.ttl()?, 100);
+    /// #
+    /// # Ok(()) }) }
+    /// ```
+    pub fn ttl(&self) -> io::Result<u32> {
+        self.0.ttl()
+    }
+
+    /// Sets the value for the `IP_TTL` option on this socket.
+    ///
+    /// This value sets the time-to-live field that is used in every packet sent
+    /// from this socket.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// # fn main() -> std::io::Result<()> { tio::task::block_on(async {
+    /// #
+    /// use tio::net::TcpStream;
+    ///
+    /// let stream = TcpStream::connect("127.0.0.1:8080").await?;
+    ///
+    /// stream.set_ttl(100)?;
+    /// assert_eq!(stream.ttl()?, 100);
+    /// #
+    /// # Ok(()) }) }
+    /// ```
+    pub fn set_ttl(&self, ttl: u32) -> io::Result<()> {
+        self.0.set_ttl(ttl)
+    }
+
+    /// Receives data on the socket from the remote address to which it is connected, without
+    /// removing that data from the queue.
+    ///
+    /// On success, returns the number of bytes peeked.
+    ///
+    /// Successive calls return the same data. This is accomplished by passing `MSG_PEEK` as a flag
+    /// to the underlying `recv` system call.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// # fn main() -> std::io::Result<()> { tio::task::block_on(async {
+    /// #
+    /// use tio::net::TcpStream;
+    ///
+    /// let stream = TcpStream::connect("127.0.0.1:8080").await?;
+    ///
+    /// let mut buf = vec![0; 1024];
+    /// let n = stream.peek(&mut buf).await?;
+    /// #
+    /// # Ok(()) }) }
+    /// ```
+    pub async fn peek(&self, buf: &mut [u8]) -> io::Result<usize> {
+        future::poll_fn(|cx| self.0.poll_read_with(cx, |inner| inner.peek(buf))).await
+    }
+
+    /// Gets the value of the `TCP_NODELAY` option on this socket.
+    ///
+    /// For more information about this option, see [`set_nodelay`].
+    ///
+    /// [`set_nodelay`]: #method.set_nodelay
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// # fn main() -> std::io::Result<()> { tio::task::block_on(async {
+    /// #
+    /// use tio::net::TcpStream;
+    ///
+    /// let stream = TcpStream::connect("127.0.0.1:8080").await?;
+    ///
+    /// stream.set_nodelay(true)?;
+    /// assert_eq!(stream.nodelay()?, true);
+    /// #
+    /// # Ok(()) }) }
+    /// ```
+    pub fn nodelay(&self) -> io::Result<bool> {
+        self.0.nodelay()
+    }
+
+    /// Sets the value of the `TCP_NODELAY` option on this socket.
+    ///
+    /// If set, this option disables the Nagle algorithm. This means that
+    /// segments are always sent as soon as possible, even if there is only a
+    /// small amount of data. When not set, data is buffered until there is a
+    /// sufficient amount to send out, thereby avoiding the frequent sending of
+    /// small packets.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// # fn main() -> std::io::Result<()> { tio::task::block_on(async {
+    /// #
+    /// use tio::net::TcpStream;
+    ///
+    /// let stream = TcpStream::connect("127.0.0.1:8080").await?;
+    ///
+    /// stream.set_nodelay(true)?;
+    /// assert_eq!(stream.nodelay()?, true);
+    /// #
+    /// # Ok(()) }) }
+    /// ```
+    pub fn set_nodelay(&self, nodelay: bool) -> io::Result<()> {
+        self.0.set_nodelay(nodelay)
+    }
+
+    /// Shuts down the read, write, or both halves of this connection.
+    ///
+    /// This method will cause all pending and future I/O on the specified portions to return
+    /// immediately with an appropriate value (see the documentation of [`Shutdown`]).
+    ///
+    /// [`Shutdown`]: https://doc.rust-lang.org/std/net/enum.Shutdown.html
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// # fn main() -> std::io::Result<()> { tio::task::block_on(async {
+    /// #
+    /// use std::net::Shutdown;
+    ///
+    /// use tio::net::TcpStream;
+    ///
+    /// let stream = TcpStream::connect("127.0.0.1:8080").await?;
+    /// stream.shutdown(Shutdown::Both)?;
+    /// #
+    /// # Ok(()) }) }
+    /// ```
+    pub fn shutdown(&self, how: std::net::Shutdown) -> std::io::Result<()> {
+        self.0.shutdown(how)
     }
 }
 
@@ -78,7 +319,7 @@ impl AsyncWrite for TcpStream {
 
     #[inline]
     fn poll_close(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<io::Result<()>> {
-        self.0.shutdown(std::net::Shutdown::Both)?;
+        self.shutdown(std::net::Shutdown::Both)?;
         Poll::Ready(Ok(()))
     }
 }
