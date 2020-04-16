95ac951432d19a12feec5ea4a5512d8726c1a1ac
Hexilee <hexileee@gmail.com>
2020-04-16 16:23:10 +0800

    add docs for net::Resolver

--- src/net/util.rs
+++ src/net/util.rs
@@ -3,9 +3,25 @@ use std::io::{self, ErrorKind::*, Result};
 use std::net::{SocketAddr, ToSocketAddrs};
 use std::task::Poll;
 
+/// An exception
 pub const WAKERS_LOCK_POISONED: &str = "wakers lock poisoned";
 
+/// Async address resolver
 pub trait Resolver: ToSocketAddrs {
+    /// Resolve address asynchronously
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # fn main() -> std::io::Result<()> { tio::task::block_on(async {
+    /// #
+    /// use tio::net::Resolver;
+    /// use std::net::SocketAddr;
+    ///
+    /// let addrs: Vec<SocketAddr> = "github.com:443".resolve().await?;
+    /// #
+    /// # Ok(()) })}
+    /// ```
     fn resolve(self) -> JoinHandle<io::Result<Vec<SocketAddr>>>;
 }
 
@@ -14,11 +30,13 @@ where
     A: 'static + Send + ToSocketAddrs,
     A::Iter: 'static + Send,
 {
+    #[inline]
     fn resolve(self) -> JoinHandle<io::Result<Vec<SocketAddr>>> {
         spawn_blocking(move || self.to_socket_addrs().map(Iterator::collect))
     }
 }
 
+/// Convert io result to poll
 #[inline]
 pub fn may_block<T>(result: Result<T>) -> Poll<Result<T>> {
     match result {
@@ -27,10 +45,11 @@ pub fn may_block<T>(result: Result<T>) -> Poll<Result<T>> {
     }
 }
 
+/// Construct invalid input io error
 #[inline]
 pub fn resolve_none() -> io::Error {
     io::Error::new(
         io::ErrorKind::InvalidInput,
-        "could not resolve to any addresses",
+        "could not resolve to any valid addresses",
     )
 }
